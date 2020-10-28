701665708ac4fcb33d264137c83c771b1827a6f6
nitipon apaisri <nitipon.apaisri@gmail.com>
2020-10-28 19:59:49 +0100

    got a concept for fecth a new link and use that link

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
-//fetch api under api
-fetch("https://swapi.dev/api/people/?page=1")
-   .then((res) => res.json())
-   .then((characters) => {
-      let cArr = [];
-      for (let i of characters.results) {
-         cArr.push(i);
-      }
-      document.querySelector(".load").addEventListener("click", () => {
-         console.log(cArr[0]);
-         fetch(characters.results[0].homeworld)
-            .then((res) => res.json())
-            .then((home) => {
-               console.log(home);
-            });
+//fetch api under api * concept
+// fetch("https://swapi.dev/api/people/?page=1")
+//    .then((res) => res.json())
+//    .then((characters) => {
+//       let cArr = [];
+//       for (let i of characters.results) {
+//          cArr.push(i);
+//       }
+//       document.querySelector(".load").addEventListener("click", () => {
+//          console.log(cArr[0]);
+//          fetch(characters.results[0].homeworld)
+//             .then((res) => res.json())
+//             .then((home) => {
+//                console.log(home);
+//             });
+//       });
+//    });
+// async function redirect() {
+//    let link = "https://swapi.dev/api/people/?page=1";
+//    document.querySelector(".next").addEventListener("click", () => {
+//       fetch(link)
+//          .then((res) => res.json())
+//          .then((pages) => {});
+//    });
+// }
+
+//Fetch the new link and get the new link *concept
+let currLink = "https://swapi.dev/api/people/?page=1";
+let newLink = "";
+document.querySelector(".next").addEventListener("click", async () => {
+   await fetch(currLink)
+      .then((res) => res.json())
+      .then((link) => {
+         newLink = link.next;
+         currLink = newLink;
       });
-   });
+   await fetch(newLink)
+      .then((res) => res.json())
+      .then((char) => {
+         console.log(char.results);
+      });
+});
