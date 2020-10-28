0688314b5930c87165e00634c734e9dd4bf781ee
nitipon apaisri <nitipon.apaisri@gmail.com>
2020-10-28 22:24:43 +0100

    almost done with ui

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 //fetch api under api * concept
-// fetch("https://swapi.dev/api/people/?page=1")
-//    .then((res) => res.json())
-//    .then((characters) => {
-//       let cArr = [];
-//       for (let i of characters.results) {
-//          cArr.push(i);
-//       }
-//       document.querySelector(".load").addEventListener("click", () => {
-//          console.log(cArr[0]);
-//          fetch(characters.results[0].homeworld)
-//             .then((res) => res.json())
-//             .then((home) => {
-//                console.log(home);
-//             });
-//       });
-//    });
-// async function redirect() {
-//    let link = "https://swapi.dev/api/people/?page=1";
-//    document.querySelector(".next").addEventListener("click", () => {
-//       fetch(link)
-//          .then((res) => res.json())
-//          .then((pages) => {});
-//    });
-// }
+fetch("https://swapi.dev/api/people/?page=1")
+   .then((res) => res.json())
+   .then((characters) => {
+      let cArr = [];
+      for (let i of characters.results) {
+         cArr.push(i);
+      }
+      document.querySelector(".load").addEventListener("click", () => {
+         console.log(cArr[0]);
+         fetch(characters.results[0].homeworld)
+            .then((res) => res.json())
+            .then((home) => {
+               console.log(home);
+            });
+      });
+   });
+let redirect = async () => {
+   let link = "https://swapi.dev/api/people/?page=1";
+   document.querySelector(".next").addEventListener("click", () => {
+      fetch(link)
+         .then((res) => res.json())
+         .then((pages) => {});
+   });
+};
 
 //Fetch the new link and get the new link *concept
 let currLink = "https://swapi.dev/api/people/?page=1";
