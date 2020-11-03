44e14f10983e325f7089e5c2900c0cf346b329d2
nitipon apaisri <nitipon.apaisri@gmail.com>
2020-11-03 09:49:08 +0100

    Revert "minor fixed"
    This reverts commit 4145c752bc6489ee6e850af4b3835ad015fc7deb.
    
    btter not chnaged

--- js/main.js
+++ js/main.js
@@ -22,25 +22,25 @@ nextBtn.addEventListener("click", async () => {
          }
          console.log(characterArr);
          let charName1 = document.querySelector(".char1");
-         charName1.innerHTML = characterArr[0].name;
+         charName1.innerHTML = characters.results[0].name;
          let charName2 = document.querySelector(".char2");
-         charName2.innerHTML = characterArr[1].name;
+         charName2.innerHTML = characters.results[1].name;
          let charName3 = document.querySelector(".char3");
-         charName3.innerHTML = characterArr[2].name;
+         charName3.innerHTML = characters.results[2].name;
          let charName4 = document.querySelector(".char4");
-         charName4.innerHTML = characterArr[3].name;
+         charName4.innerHTML = characters.results[3].name;
          let charName5 = document.querySelector(".char5");
-         charName5.innerHTML = characterArr[4].name;
+         charName5.innerHTML = characters.results[4].name;
          let charName6 = document.querySelector(".char6");
-         charName6.innerHTML = characterArr[5].name;
+         charName6.innerHTML = characters.results[5].name;
          let charName7 = document.querySelector(".char7");
-         charName7.innerHTML = characterArr[6].name;
+         charName7.innerHTML = characters.results[6].name;
          let charName8 = document.querySelector(".char8");
-         charName8.innerHTML = characterArr[7].name;
+         charName8.innerHTML = characters.results[7].name;
          let charName9 = document.querySelector(".char9");
-         charName9.innerHTML = characterArr[8].name;
+         charName9.innerHTML = characters.results[8].name;
          let charName10 = document.querySelector(".char10");
-         charName10.innerHTML = characterArr[9].name;
+         charName10.innerHTML = characters.results[9].name;
          for (let y of allClick) {
             y.addEventListener("click", () => {
                hideCharacterDetails();
@@ -51,38 +51,38 @@ nextBtn.addEventListener("click", async () => {
                   hidePreLoaderWorldDetails();
                   showCharacterDetails();
                   let name = document.querySelector(".name");
-                  name.innerHTML = `Name: ${characterArr[y.value].name}`;
+                  name.innerHTML = "Name: " + characters.results[y.value].name;
                   let height = document.querySelector(".height");
-                  height.innerHTML = `Height: ${characterArr[y.value].height}`;
+                  height.innerHTML = "Height: " + characters.results[y.value].height;
                   let mass = document.querySelector(".mass");
-                  mass.innerHTML = `Mass: ${characterArr[y.value].mass}`;
+                  mass.innerHTML = "Mass: " + characters.results[y.value].mass;
                   let hair = document.querySelector(".hair");
-                  hair.innerHTML = `Hair: ${characterArr[y.value].hair_color}`;
+                  hair.innerHTML = "Hair: " + characters.results[y.value].hair_color;
                   let skin = document.querySelector(".skin");
-                  skin.innerHTML = `Skin: ${characterArr[y.value].skin_color}`;
+                  skin.innerHTML = "Skin: " + characters.results[y.value].skin_color;
                   let eye = document.querySelector(".eye");
-                  eye.innerHTML = `Eye: ${characterArr[y.value].eye_color}`;
+                  eye.innerHTML = "Eye: " + characters.results[y.value].eye_color;
                   let bday = document.querySelector(".bday");
-                  bday.innerHTML = `Birth year: ${characterArr[y.value].birth_year}`;
+                  bday.innerHTML = "Birth year: " + characters.results[y.value].birth_year;
                   let gender = document.querySelector(".gender");
-                  gender.innerHTML = `Gender: ${characterArr[y.value].gender}`;
+                  gender.innerHTML = "Gender: " + characters.results[y.value].gender;
                }, 1500);
-               fetch(characterArr[y.value].homeworld)
+               fetch(characters.results[y.value].homeworld)
                   .then((res) => res.json())
                   .then((home) => {
                      console.log(home);
                      let planet = document.querySelector(".planet");
                      planet.innerText = home.name;
                      let rotation = document.querySelector(".rotation");
-                     rotation.innerText = `Rotation period: ${home.rotation_period} h`;
+                     rotation.innerText = "Rotation period: " + home.rotation_period + " h";
                      let orbit = document.querySelector(".orbit");
-                     orbit.innerText = `Orbit period: ${home.orbital_period} days`;
+                     orbit.innerText = "Orbit period: " + home.orbital_period + " days";
                      let diameter = document.querySelector(".diameter");
-                     diameter.innerText = `Diameter: ${home.diameter} km`;
+                     diameter.innerText = "Diameter: " + home.diameter + " km";
                      let gravity = document.querySelector(".gravity");
-                     gravity.innerText = `Gravity: ${home.gravity} standard`;
+                     gravity.innerText = "Gravity: " + home.gravity + " standard";
                      let terrain = document.querySelector(".terrain");
-                     terrain.innerText = `Terrain: ${home.terrain}`;
+                     terrain.innerText = "Terrain: " + home.terrain;
                   });
             });
          }
@@ -108,27 +108,27 @@ backBtn.addEventListener("click", async () => {
          }
          console.log(characterArr);
          let charName1 = document.querySelector(".char1");
-         charName1.innerHTML = characterArr[0].name;
+         charName1.innerHTML = characters.results[0].name;
          let charName2 = document.querySelector(".char2");
-         charName2.innerHTML = characterArr[1].name;
+         charName2.innerHTML = characters.results[1].name;
          let charName3 = document.querySelector(".char3");
-         charName3.innerHTML = characterArr[2].name;
+         charName3.innerHTML = characters.results[2].name;
          let charName4 = document.querySelector(".char4");
-         charName4.innerHTML = characterArr[3].name;
+         charName4.innerHTML = characters.results[3].name;
          let charName5 = document.querySelector(".char5");
-         charName5.innerHTML = characterArr[4].name;
+         charName5.innerHTML = characters.results[4].name;
          let charName6 = document.querySelector(".char6");
-         charName6.innerHTML = characterArr[5].name;
+         charName6.innerHTML = characters.results[5].name;
          let charName7 = document.querySelector(".char7");
-         charName7.innerHTML = characterArr[6].name;
+         charName7.innerHTML = characters.results[6].name;
          let charName8 = document.querySelector(".char8");
-         charName8.innerHTML = characterArr[7].name;
+         charName8.innerHTML = characters.results[7].name;
          let charName9 = document.querySelector(".char9");
-         charName9.innerHTML = characterArr[8].name;
+         charName9.innerHTML = characters.results[8].name;
          let charName10 = document.querySelector(".char10");
-         charName10.innerHTML = characterArr[9].name;
-         for (let y of allClick) {
-            y.addEventListener("click", () => {
+         charName10.innerHTML = characters.results[9].name;
+         for (let i of allClick) {
+            i.addEventListener("click", () => {
                hideCharacterDetails();
                showPreLoaderWorldDetails();
                showPreLoaderDetails();
@@ -137,37 +137,37 @@ backBtn.addEventListener("click", async () => {
                   hidePreLoaderWorldDetails();
                   showCharacterDetails();
                   let name = document.querySelector(".name");
-                  name.innerHTML = `Name: ${characterArr[y.value].name}`;
+                  name.innerHTML = "Name: " + characters.results[i.value].name;
                   let height = document.querySelector(".height");
-                  height.innerHTML = `Height: ${characterArr[y.value].height}`;
+                  height.innerHTML = "Height: " + characters.results[i.value].height;
                   let mass = document.querySelector(".mass");
-                  mass.innerHTML = `Mass: ${characterArr[y.value].mass}`;
+                  mass.innerHTML = "Mass: " + characters.results[i.value].mass;
                   let hair = document.querySelector(".hair");
-                  hair.innerHTML = `Hair: ${characterArr[y.value].hair_color}`;
+                  hair.innerHTML = "Hair: " + characters.results[i.value].hair_color;
                   let skin = document.querySelector(".skin");
-                  skin.innerHTML = `Skin: ${characterArr[y.value].skin_color}`;
+                  skin.innerHTML = "Skin: " + characters.results[i.value].skin_color;
                   let eye = document.querySelector(".eye");
-                  eye.innerHTML = `Eye: ${characterArr[y.value].eye_color}`;
+                  eye.innerHTML = "Eye: " + characters.results[i.value].eye_color;
                   let bday = document.querySelector(".bday");
-                  bday.innerHTML = `Birth year: ${characterArr[y.value].birth_year}`;
+                  bday.innerHTML = "Birth year: " + characters.results[i.value].birth_year;
                   let gender = document.querySelector(".gender");
-                  gender.innerHTML = `Gender: ${characterArr[y.value].gender}`;
-                  fetch(characterArr[i.value].homeworld)
+                  gender.innerHTML = "Gender: " + characters.results[i.value].gender;
+                  fetch(characters.results[i.value].homeworld)
                      .then((res) => res.json())
                      .then((home) => {
                         console.log(home);
                         let planet = document.querySelector(".planet");
                         planet.innerText = home.name;
                         let rotation = document.querySelector(".rotation");
-                        rotation.innerText = `Rotation period: ${home.rotation_period} h`;
+                        rotation.innerText = "Rotation period: " + home.rotation_period + " h";
                         let orbit = document.querySelector(".orbit");
-                        orbit.innerText = `Orbit period: ${home.orbital_period} days`;
+                        orbit.innerText = "Orbit period: " + home.orbital_period + " days";
                         let diameter = document.querySelector(".diameter");
-                        diameter.innerText = `Diameter: ${home.diameter} km`;
+                        diameter.innerText = "Diameter: " + home.diameter + " km";
                         let gravity = document.querySelector(".gravity");
-                        gravity.innerText = `Gravity: ${home.gravity} standard`;
+                        gravity.innerText = "Gravity: " + home.gravity + " standard";
                         let terrain = document.querySelector(".terrain");
-                        terrain.innerText = `Terrain: ${home.terrain}`;
+                        terrain.innerText = "Terrain: " + home.terrain;
                      });
                }, 1500);
             });
