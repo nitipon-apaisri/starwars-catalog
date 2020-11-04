a5fdb6a86c62a293c8ef38444181dbc4e060f995
nitipon apaisri <nitipon.apaisri@gmail.com>
2020-11-04 20:48:37 +0100

    added icons and changed some variables from let to const

--- js/func.js
+++ js/func.js
@@ -1,57 +1,56 @@
-let increasePage = () => {
+const increasePage = () => {
    count++;
-   let pageNr = document.querySelector("main > .container > .characters > .btn > p");
+   const pageNr = document.querySelector("main > .container > .characters > .btn > p");
    pageNr.innerHTML = count + "/8";
    console.log(count);
 };
-let decreasePage = () => {
+const decreasePage = () => {
    count--;
-   let pageNr = document.querySelector("main > .container > .characters > .btn > p");
+   const pageNr = document.querySelector("main > .container > .characters > .btn > p");
    pageNr.innerHTML = count + "/8";
    console.log(count);
-   return count;
 };
-let hideCharacterList = () => {
-   let cList = document.querySelector(".cList");
+const hideCharacterList = () => {
+   const cList = document.querySelector(".cList");
    cList.classList.add("hide");
 };
-let hideCharacterDetails = () => {
-   let cSelfInfo = document.querySelector(".self-info > ul");
+const hideCharacterDetails = () => {
+   const cSelfInfo = document.querySelector(".self-info > ul");
    cSelfInfo.classList.add("hide");
-   let cHome = document.querySelector(".home-world-info > ul");
+   const cHome = document.querySelector(".home-world-info > ul");
    cHome.classList.add("hide");
 };
-let showCharacterList = () => {
-   let cList = document.querySelector(".cList");
+const showCharacterList = () => {
+   const cList = document.querySelector(".cList");
    cList.classList.remove("hide");
 };
-let showCharacterDetails = () => {
-   let cSelfInfo = document.querySelector(".self-info > ul");
+const showCharacterDetails = () => {
+   const cSelfInfo = document.querySelector(".self-info > ul");
    cSelfInfo.classList.remove("hide");
-   let cHome = document.querySelector(".home-world-info > ul");
+   const cHome = document.querySelector(".home-world-info > ul");
    cHome.classList.remove("hide");
 };
-let showPreLoaderChracter = () => {
-   let preLoader = document.querySelector(".loader-chracter");
+const showPreLoaderChracter = () => {
+   const preLoader = document.querySelector(".loader-chracter");
    preLoader.classList.remove("hide");
 };
-let hidePreLoaderChracter = () => {
-   let preLoader = document.querySelector(".loader-chracter");
+const hidePreLoaderChracter = () => {
+   const preLoader = document.querySelector(".loader-chracter");
    preLoader.classList.add("hide");
 };
-let showPreLoaderDetails = () => {
-   let preLoader = document.querySelector(".loader-details");
+const showPreLoaderDetails = () => {
+   const preLoader = document.querySelector(".loader-details");
    preLoader.classList.remove("hide");
 };
-let hidePreLoaderDetails = () => {
-   let preLoader = document.querySelector(".loader-details");
+const hidePreLoaderDetails = () => {
+   const preLoader = document.querySelector(".loader-details");
    preLoader.classList.add("hide");
 };
-let showPreLoaderWorldDetails = () => {
-   let preLoader = document.querySelector(".loader-world-details");
+const showPreLoaderWorldDetails = () => {
+   const preLoader = document.querySelector(".loader-world-details");
    preLoader.classList.remove("hide");
 };
-let hidePreLoaderWorldDetails = () => {
-   let preLoader = document.querySelector(".loader-world-details");
+const hidePreLoaderWorldDetails = () => {
+   const preLoader = document.querySelector(".loader-world-details");
    preLoader.classList.add("hide");
 };
